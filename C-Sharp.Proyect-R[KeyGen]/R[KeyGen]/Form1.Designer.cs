ff346a48763e6c4b5c26d1839e243f17cb857e2c
IbizaHerrero <ibzx1992@gmail.com>
2015-09-12 16:02:41 +0200

    Generador de claves aleatorias con distintos parámetros de salida (Base64, Hex, ASCII y SHA256 con Base64).

---
+++ C-Sharp.Proyect-R[KeyGen]/R[KeyGen]/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Random_KeyGen_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clave generada:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(365, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Número de caracteres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tipo de caracteres:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(15, 128);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "0...9";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(97, 128);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "abc...xyz";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.White;
+            this.radioButton3.Location = new System.Drawing.Point(187, 128);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(76, 17);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "ABC...XYZ";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.White;
+            this.radioButton4.Location = new System.Drawing.Point(287, 128);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(89, 17);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "º!\"·$%&/()*+,-./";
+            this.radioButton4.UseVisualStyleBackColor = false;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.BackColor = System.Drawing.Color.White;
+            this.radioButton5.Location = new System.Drawing.Point(15, 151);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(66, 17);
+            this.radioButton5.TabIndex = 14;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = ":;<=>?@";
+            this.radioButton5.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.Text = "8";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Tipo de salida:";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.BackColor = System.Drawing.Color.White;
+            this.radioButton6.Location = new System.Drawing.Point(287, 151);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(51, 17);
+            this.radioButton6.TabIndex = 22;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "FULL";
+            this.radioButton6.UseVisualStyleBackColor = false;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.BackColor = System.Drawing.Color.White;
+            this.radioButton7.Location = new System.Drawing.Point(187, 151);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(42, 17);
+            this.radioButton7.TabIndex = 21;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "{|}~";
+            this.radioButton7.UseVisualStyleBackColor = false;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.BackColor = System.Drawing.Color.White;
+            this.radioButton8.Location = new System.Drawing.Point(97, 151);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(51, 17);
+            this.radioButton8.TabIndex = 20;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "[\\]^_`";
+            this.radioButton8.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(15, 187);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "BASE64";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(97, 187);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 17);
+            this.checkBox2.TabIndex = 24;
+            this.checkBox2.Text = "HEX";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.Color.White;
+            this.checkBox3.Location = new System.Drawing.Point(187, 187);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(53, 17);
+            this.checkBox3.TabIndex = 25;
+            this.checkBox3.Text = "ASCII";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.BackColor = System.Drawing.Color.White;
+            this.checkBox4.Location = new System.Drawing.Point(287, 187);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(54, 17);
+            this.checkBox4.TabIndex = 26;
+            this.checkBox4.Text = "SHA1";
+            this.checkBox4.UseVisualStyleBackColor = false;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(388, 212);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RKEYGEN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ C-Sharp.Proyect-R[KeyGen]/R[KeyGen]/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace Random_KeyGen_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenKey();
+        }
+
+        //#Genera la clave segun los parámetros determinados.
+        private void GenKey()
+        {
+            try
+            {
+                int keyLength = Convert.ToInt32(textBox2.Text);
+
+                char[] chrArry = new char[keyLength];
+                Random Rnd = new Random(DateTime.UtcNow.Millisecond);
+
+                for (int i = 0; i < keyLength; i++)
+                {
+                    if (radioButton1.Checked)
+                    {                        
+                        Thread.Sleep(Rnd.Next(0,200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(48, 58));
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0,200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(97,123));
+                    }       
+                    else if (radioButton3.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(65, 91));
+                    }
+                    else if (radioButton4.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(32, 47));
+                    }
+                    else if (radioButton5.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(58, 64));
+                    }
+                    else if (radioButton6.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(91, 96));
+                    }
+                    else if (radioButton7.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(123, 127));
+                    }
+                    else if (radioButton8.Checked)
+                    {
+                        Thread.Sleep(Rnd.Next(0, 200));
+                        chrArry[i] = Convert.ToChar(Rnd.Next(0, 256));
+                    }
+                }
+
+                OutKeyFormat(chrArry);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("Error: No se ha introducido el tamaño de la cadena a generar.\r\n" + E.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    
+            private void OutKeyFormat(char[] ckey)
+            {
+                if (checkBox1.Checked)
+                {
+                    textBox1.Text = Convert.ToBase64String(Encoding.UTF8.GetBytes(ckey), Base64FormattingOptions.None);
+                }
+                else if (checkBox2.Checked)
+                {
+                    List<string> strKeyHex = new List<string>();
+
+                    for (int i = 0; i < 32; i++)
+                    {
+                        int chr = ckey[i];
+                        strKeyHex.Add(chr.ToString("X"));
+                    }    
+                    
+                    textBox1.Text = string.Join("", strKeyHex);
+                }
+                else if (checkBox3.Checked)
+                {
+                    string strKeyAscii = new string(ckey);
+                    textBox1.Text = strKeyAscii;
+                }
+                else if (checkBox4.Checked)
+                {
+                    SHA1Managed SHA256 = new SHA1Managed();
+                    SHA256.ComputeHash(Encoding.UTF8.GetBytes(ckey), 0, ckey.Length);
+                    textBox1.Text = Convert.ToBase64String(SHA256.Hash, Base64FormattingOptions.None).Substring(0, Convert.ToInt32(textBox2.Text));
+                }
+            }
+
+            private void checkBox1_CheckedChanged(object sender, EventArgs e)
+            {
+                if (checkBox1.Checked)
+                {
+                    checkBox2.Enabled = false; checkBox3.Enabled = false; checkBox4.Enabled = false;
+                }
+                else
+                {
+                    checkBox2.Enabled = true; checkBox3.Enabled = true; checkBox4.Enabled = true;
+
+                }
+            }
+
+            private void checkBox2_CheckedChanged(object sender, EventArgs e)
+            {
+                if (checkBox2.Checked)
+                {
+                    checkBox1.Enabled = false; checkBox3.Enabled = false; checkBox4.Enabled = false;
+                }
+                else
+                {
+                    checkBox1.Enabled = true; checkBox3.Enabled = true; checkBox4.Enabled = true;
+
+                }
+            }
+
+            private void checkBox3_CheckedChanged(object sender, EventArgs e)
+            {
+                if (checkBox3.Checked)
+                {
+                    checkBox1.Enabled = false; checkBox2.Enabled = false; checkBox4.Enabled = false;
+                }
+                else
+                {
+                    checkBox1.Enabled = true; checkBox2.Enabled = true; checkBox4.Enabled = true;
+
+                }
+            }
+
+            private void checkBox4_CheckedChanged(object sender, EventArgs e)
+            {
+                if (checkBox4.Checked)
+                {
+                    checkBox1.Enabled = false; checkBox2.Enabled = false; checkBox3.Enabled = false;
+                }
+                else
+                {
+                    checkBox1.Enabled = true; checkBox2.Enabled = true; checkBox3.Enabled = true;
+
+                }
+            }
+
+            private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                
+            }
+        }
+    }
+
